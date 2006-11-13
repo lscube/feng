475a159dd91cb4bac32f46133e70f7b8cde92671
lu_zero <lu_zero>
2006-11-13 16:52:09 +0000

    minor update

--- include/fenice/eventloop.h
+++ include/fenice/eventloop.h
@@ -35,16 +35,17 @@
 #ifndef _EVENTLOOPH
 #define _EVENTLOOPH
 
-	#include <fenice/wsocket.h>	
-	#include <fenice/rtsp.h>
+#include <netembryo/wsocket.h>
+#include <fenice/rtsp.h>
 
-	#define MAX_FDS 800
-	
-	typedef int (*event_function)(void *data);
-	
-	void eventloop(Sock *m_fd);
-	int rtsp_server(RTSP_buffer *rtsp);
-	void add_client(RTSP_buffer **rtsp_list, Sock *s_fd);
-	void schedule_connections(RTSP_buffer **rtsp_list, int *conn_count);
-	
+#define MAX_FDS 800
+
+typedef int (*event_function) (void *data);
+
+void eventloop(tsocket main_fd, tsocket main_sctp_fd);
+int rtsp_server(RTSP_buffer * rtsp, fd_set * rset, fd_set * wset, fd_set * xset);
+void add_client(RTSP_buffer ** rtsp_list, tsocket fd, RTSP_proto proto);
+void schedule_connections(RTSP_buffer ** rtsp_list, int *conn_count, fd_set * rset, fd_set * wset, fd_set * xset);
+void rtsp_set_fdsets(RTSP_buffer * rtsp, int * max_fd , fd_set * rset, fd_set * wset, fd_set * xset);
+
 #endif
