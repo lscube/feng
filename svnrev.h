f5221133e082d65f815f4080a0811cbdf27a8716
shawill <shawill>
2006-11-30 11:53:39 +0000

    default config file is now feng.conf

--- svnrev.h
+++ svnrev.h
@@ -1,7 +1,7 @@
 /* This header is just for keeping track of current SVN revision.
  * If this file is present in the sources tree then this is a development SVN
  * version.
- * Thu Nov 30 11:53:37 CET 2006
+ * Thu Nov 30 12:53:12 CET 2006
  * */
 
 #ifndef __SVNREV_H
