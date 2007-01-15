58c6609653b15673bd3d69c63c9899535fe8081b
lu_zero <lu_zero>
2007-01-15 07:37:09 +0000

    Cosmetics part 1 (first step in order to gain dss compatibility)

--- sdp2/sdp_media_descr.c
+++ sdp2/sdp_media_descr.c
@@ -32,18 +32,21 @@
 #include <fenice/sdp2.h>
 #include <fenice/utils.h>
 
-#define DESCRCAT(x) do { \
-						if ( (size_left -= x) < 0) \
-						return ERR_INPUT_PARAM; \
-						else cursor=descr+descr_size-size_left; \
-					} while(0);
-int sdp_media_descr(ResourceDescr *r_descr, MediaDescrList m_descr_list, char *descr, uint32 descr_size)
+#define DESCRCAT(x) \
+    do { \
+        if ( (size_left -= x) < 0) \
+	    return ERR_INPUT_PARAM; \
+        else cursor=descr+descr_size-size_left; \
+    } while(0);
+
+int sdp_media_descr(ResourceDescr *r_descr, MediaDescrList m_descr_list,
+                    char *descr, uint32 descr_size)
 {
 	MediaDescr *m_descr = m_descr_list ? MEDIA_DESCR(m_descr_list) : NULL;
 	MediaDescrList tmp_mdl;
 	sdp_field_list sdp_private;
-	gint64 size_left=descr_size;
-	char *cursor=descr;
+	gint64 size_left = descr_size;
+	char *cursor = descr;
 	
 	if (!m_descr)
 		return ERR_INPUT_PARAM;
@@ -69,10 +72,16 @@ int sdp_media_descr(ResourceDescr *r_descr, MediaDescrList m_descr_list, char *d
 			break;
 	}
 
-	// shawill: TODO: probably the transport should not be hard coded, but obtained in some way
-	DESCRCAT(g_snprintf(cursor, size_left, "%d RTP/AVP", m_descr_rtp_port(m_descr) ))
-	for (tmp_mdl = list_first(m_descr_list); tmp_mdl; tmp_mdl=list_next(tmp_mdl))
-		DESCRCAT(g_snprintf(cursor, size_left, " %u", m_descr_rtp_pt(MEDIA_DESCR(tmp_mdl))))
+	// shawill: TODO: probably the transport should not be hard coded,
+        // but obtained in some way
+        DESCRCAT(g_snprintf(cursor, size_left, "%d RTP/AVP",
+                    m_descr_rtp_port(m_descr) ))
+	for (tmp_mdl = list_first(m_descr_list);
+             tmp_mdl;
+             tmp_mdl=list_next(tmp_mdl))
+		DESCRCAT(g_snprintf(cursor, size_left, " %u",
+                            m_descr_rtp_pt(MEDIA_DESCR(tmp_mdl))))
+
 	DESCRCAT(g_strlcat(cursor, SDP2_EL, size_left))
 	
 	// i=*
@@ -80,11 +89,16 @@ int sdp_media_descr(ResourceDescr *r_descr, MediaDescrList m_descr_list, char *d
 	// b=*
 	// k=*
 	// a=*
-	DESCRCAT(g_snprintf(cursor, size_left, "a=control:%s!%s"SDP2_EL, r_descr_name(r_descr), m_descr_name(m_descr)))
+	DESCRCAT(g_snprintf(cursor, size_left, "a=control:%s!%s"SDP2_EL,
+            r_descr_name(r_descr), m_descr_name(m_descr)))
 	// other sdp private data
-	for (tmp_mdl = list_first(m_descr_list); tmp_mdl; tmp_mdl=list_next(tmp_mdl))
+	for (tmp_mdl = list_first(m_descr_list);
+            tmp_mdl;
+            tmp_mdl=list_next(tmp_mdl))
 	if ( (sdp_private=m_descr_sdp_private(MEDIA_DESCR(tmp_mdl))) )
-		for (sdp_private = list_first(sdp_private); sdp_private; sdp_private = list_next(sdp_private)) {
+		for (sdp_private = list_first(sdp_private);
+                    sdp_private;
+                    sdp_private = list_next(sdp_private)) {
 			switch (SDP_FIELD(sdp_private)->type) {
 				case empty_field:
 					DESCRCAT(g_snprintf(cursor, size_left, "%s"SDP2_EL, SDP_FIELD(sdp_private)->field))
