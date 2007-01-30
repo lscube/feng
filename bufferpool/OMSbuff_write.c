a23b926629061dfa89015b646bed2563ce22465f
lu_zero <lu_zero>
2007-01-30 17:17:39 +0000

    Cosmetics

--- bufferpool/OMSbuff_write.c
+++ bufferpool/OMSbuff_write.c
@@ -6,15 +6,15 @@
  *  Fenice -- Open Media Server
  *
  *  Copyright (C) 2004 by
- *  	
- *	- Giampaolo Mancini	<giampaolo.mancini@polito.it>
- *	- Francesco Varano	<francesco.varano@polito.it>
- *	- Marco Penno		<marco.penno@polito.it>
- *	- Federico Ridolfo	<federico.ridolfo@polito.it>
- *	- Eugenio Menegatti 	<m.eu@libero.it>
- *	- Stefano Cau
- *	- Giuliano Emma
- *	- Stefano Oldrini
+ *      
+ *    - Giampaolo Mancini    <giampaolo.mancini@polito.it>
+ *    - Francesco Varano    <francesco.varano@polito.it>
+ *    - Marco Penno        <marco.penno@polito.it>
+ *    - Federico Ridolfo    <federico.ridolfo@polito.it>
+ *    - Eugenio Menegatti     <m.eu@libero.it>
+ *    - Stefano Cau
+ *    - Giuliano Emma
+ *    - Stefano Oldrini
  * 
  *  Fenice is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -50,64 +50,64 @@
  *  \return ERR_NOERROR or ERR_ALLOC
  *  */
 int OMSbuff_write(OMSBuffer * buffer, uint64 seq, uint32 timestamp,
-		    uint8 marker, uint8 * data, uint32 data_size)
+            uint8 marker, uint8 * data, uint32 data_size)
 {
-	OMSSlot *slot = &buffer->slots[buffer->control->write_pos];
-	uint64 curr_seq = slot->slot_seq;
-	OMSSlot *valid_read_pos =
-	    &buffer->slots[buffer->control->valid_read_pos];
-	double ts;
+    OMSSlot *slot = &buffer->slots[buffer->control->write_pos];
+    uint64 curr_seq = slot->slot_seq;
+    OMSSlot *valid_read_pos =
+        &buffer->slots[buffer->control->valid_read_pos];
+    double ts;
 
-	OMSbuff_lock(buffer);
+    OMSbuff_lock(buffer);
 
-	if (OMSbuff_shm_refresh(buffer))
-		return ERR_ALLOC;
+    if (OMSbuff_shm_refresh(buffer))
+        return ERR_ALLOC;
 
-	if (buffer->slots[slot->next].data == data) {
-		slot = &buffer->slots[slot->next];
-	} else {
-		if (buffer->slots[slot->next].refs > 0) {
-			if ((slot = OMSbuff_addpage(buffer, slot)) == NULL) {
-				OMSbuff_unlock(buffer);
-				return ERR_ALLOC;
-			}
-		} else {
-			slot = &buffer->slots[slot->next];
-			// write_pos reaches valid_read_pos, we "push" it
-			if ((valid_read_pos->slot_seq)
-			    && (valid_read_pos == slot)) {
-				for (ts = valid_read_pos->timestamp;
-				     (buffer->slots[valid_read_pos->next].
-				      slot_seq)
-				     && (ts == valid_read_pos->timestamp);
-				     ts =
-				     valid_read_pos->timestamp, valid_read_pos =
-				     &buffer->slots[valid_read_pos->next]);
-				buffer->control->valid_read_pos =
-				    OMStoSlotPtr(buffer, valid_read_pos);
-			}
-		}
+    if (buffer->slots[slot->next].data == data) {
+        slot = &buffer->slots[slot->next];
+    } else {
+        if (buffer->slots[slot->next].refs > 0) {
+            if ((slot = OMSbuff_addpage(buffer, slot)) == NULL) {
+                OMSbuff_unlock(buffer);
+                return ERR_ALLOC;
+            }
+        } else {
+            slot = &buffer->slots[slot->next];
+            // write_pos reaches valid_read_pos, we "push" it
+            if ((valid_read_pos->slot_seq)
+                && (valid_read_pos == slot)) {
+                for (ts = valid_read_pos->timestamp;
+                     (buffer->slots[valid_read_pos->next].
+                      slot_seq)
+                     && (ts == valid_read_pos->timestamp);
+                     ts =
+                     valid_read_pos->timestamp, valid_read_pos =
+                     &buffer->slots[valid_read_pos->next]);
+                buffer->control->valid_read_pos =
+                    OMStoSlotPtr(buffer, valid_read_pos);
+            }
+        }
 
-		memcpy(slot->data, data, data_size);
-	}
+        memcpy(slot->data, data, data_size);
+    }
 
-	slot->timestamp = timestamp;
-	slot->marker = marker;
-	slot->data_size = data_size;
+    slot->timestamp = timestamp;
+    slot->marker = marker;
+    slot->data_size = data_size;
 
-	slot->refs = buffer->control->refs;
-	slot->slot_seq = seq ? seq : curr_seq + 1;
+    slot->refs = buffer->control->refs;
+    slot->slot_seq = seq ? seq : curr_seq + 1;
 
-	buffer->control->write_pos = OMStoSlotPtr(buffer, slot);
+    buffer->control->write_pos = OMStoSlotPtr(buffer, slot);
 
 //      if ( msync(slot, sizeof(OMSSlot), MS_ASYNC) )
 //      if ( msync(buffer->slots, buffer->known_slots*sizeof(OMSSlot), MS_ASYNC) )
 //              printf("*** slot msync error\n");
 //      if ( msync(buffer->control, sizeof(OMSControl), MS_ASYNC) )
 //              printf("*** control msync error\n");
-	OMSbuff_dump(NULL, buffer);
+    OMSbuff_dump(NULL, buffer);
 
-	OMSbuff_unlock(buffer);
+    OMSbuff_unlock(buffer);
 
-	return ERR_NOERROR;
+    return ERR_NOERROR;
 }
