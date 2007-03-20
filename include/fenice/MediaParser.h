ca5a873fe12c53b9ca5a9de01d604b0c049f19d0
lu_zero <lu_zero>
2007-03-20 16:25:18 +0000

    Cleanup - Part 5 of 1000

--- include/fenice/MediaParser.h
+++ include/fenice/MediaParser.h
@@ -6,11 +6,11 @@
  *  Fenice -- Open Media Server
  *
  *  Copyright (C) 2004 by
- *  	
- *	- Giampaolo Mancini	<giampaolo.mancini@polito.it>
- *	- Francesco Varano	<francesco.varano@polito.it>
- *	- Marco Penno		<marco.penno@polito.it>
- *	- Federico Ridolfo	<federico.ridolfo@polito.it>
+ *      
+ *    - Giampaolo Mancini    <giampaolo.mancini@polito.it>
+ *    - Francesco Varano    <francesco.varano@polito.it>
+ *    - Marco Penno        <marco.penno@polito.it>
+ *    - Federico Ridolfo    <federico.ridolfo@polito.it>
  * 
  *  Fenice is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -46,58 +46,56 @@ typedef enum {mc_undefined=-1, mc_frame=0, mc_sample=1} MediaCodingType;
 
 typedef enum {MP_undef=-1, MP_audio, MP_video, MP_application, MP_data, MP_control} MediaType;
 
-#if 0 // define MObject with MObject_def
-typedef struct {
-	MOBJECT_COMMONS; // MObject commons MUST be the first field
-#endif
 MObject_def(__MEDIA_PROPERTIES)
-	int bit_rate; /*average if VBR or -1 is not usefull*/
-	MediaCodingType coding_type;
-	int payload_type;
-	unsigned int clock_rate;
-	char encoding_name[11];
-	MediaType media_type;
-        double mtime;    //FIXME Move to bufferpool   //time is in seconds
-        double duration; //FIXME Move to bufferpool
-	// Audio specific properties:
-	float sample_rate;/*SamplingFrequency*/
-	float OutputSamplingFrequency;
-	int audio_channels;
-	int bit_per_sample;/*BitDepth*/
-	// Video specific properties:
-	int frame_rate;
-	// more specific video information
-	int FlagInterlaced;
-	//short StereoMode;
-	unsigned int PixelWidth;
-	unsigned int PixelHeight;
-	unsigned int DisplayWidth;
-	unsigned int DisplayHeight;
-	unsigned int DisplayUnit;
-	unsigned int AspectRatio;
-	uint8_t *ColorSpace;
-	float GammaValue;
-        uint8_t *extradata;
-        long extradata_len;
-	sdp_field_list sdp_private;
+    int bit_rate; /*! average if VBR or -1 is not useful*/
+    MediaCodingType coding_type;
+    int payload_type;
+    unsigned int clock_rate;
+    char encoding_name[11];
+    MediaType media_type;
+    double mtime;    //FIXME Move to bufferpool   //time is in seconds
+    double duration; //FIXME Move to bufferpool
+    //! Audio specific properties:
+    //! \defgroup Audio Properties
+    //\@{
+    float sample_rate;/*! SamplingFrequency*/
+    float OutputSamplingFrequency;
+    int audio_channels;
+    int bit_per_sample;/*! BitDepth*/
+    //\@}
+    //! Video specific properties:
+    //! \defgroup Video Properties
+    //\@{
+    int frame_rate;
+    int FlagInterlaced;
+    unsigned int PixelWidth;
+    unsigned int PixelHeight;
+    unsigned int DisplayWidth;
+    unsigned int DisplayHeight;
+    unsigned int DisplayUnit;
+    unsigned int AspectRatio;
+    uint8_t *ColorSpace;
+    float GammaValue;
+    //\@}
+    uint8_t *extradata;
+    long extradata_len;
+    sdp_field_list sdp_private;
 } MediaProperties;
 
 typedef struct {
-	const char *encoding_name; /*i.e. MPV, MPA ...*/
-	const MediaType media_type;
+    const char *encoding_name; /*i.e. MPV, MPA ...*/
+    const MediaType media_type;
 } MediaParserInfo;
 
 typedef struct __MEDIAPARSER {
-	MediaParserInfo *info;
-	int (*init)(MediaProperties *,void **); // shawill: TODO: specify right parameters
-	int (*get_frame)(uint8 *, uint32, double *, InputStream *, MediaProperties *, void *);
-	int (*packetize)(uint8 *, uint32 *, uint8 *, uint32, MediaProperties *, void *);
-        int (*parse)(void *track, // workaround
-                     uint8 *data,
-                     long len,
-                     uint8 *extradata,
-                     long extradata_len);
-	int (*uninit)(void *);
+    MediaParserInfo *info;
+    int (*init)(MediaProperties *,void **); // shawill: TODO: specify right parameters
+    int (*get_frame)(uint8 *, uint32, double *, InputStream *,
+                     MediaProperties *, void *);
+    int (*packetize)(uint8 *, uint32 *, uint8 *, uint32, MediaProperties *, void *);
+    int (*parse)(void *track, uint8 *data, long len, uint8 *extradata,
+                 long extradata_len);
+    int (*uninit)(void *);
 } MediaParser;
 
 /*MediaParser Interface*/
@@ -105,7 +103,6 @@ void free_parser(MediaParser *);
 MediaParser *add_media_parser(void); 
 MediaParser *mparser_find(const char *);
 void mparser_unreg(MediaParser *, void *);
-// int set_media_entity(MediaParserType *, char *encoding_name);
 
 #define DEFAULT_MTU 1440
 
