3f2d43a486c6cad63122f46c1116a6ecf53fedd1
lu_zero <lu_zero>
2007-03-22 20:27:53 +0000

    Cosmetics

--- rtsp/send_setup_reply.c
+++ rtsp/send_setup_reply.c
@@ -6,15 +6,15 @@
  *  Fenice -- Open Media Server
  *
  *  Copyright (C) 2004 by
- *  	
- *	- Giampaolo Mancini	<giampaolo.mancini@polito.it>
- *	- Francesco Varano	<francesco.varano@polito.it>
- *	- Marco Penno		<marco.penno@polito.it>
- *	- Federico Ridolfo	<federico.ridolfo@polito.it>
- *	- Eugenio Menegatti 	<m.eu@libero.it>
- *	- Stefano Cau
- *	- Giuliano Emma
- *	- Stefano Oldrini
+ *      
+ *    - Giampaolo Mancini    <giampaolo.mancini@polito.it>
+ *    - Francesco Varano    <francesco.varano@polito.it>
+ *    - Marco Penno        <marco.penno@polito.it>
+ *    - Federico Ridolfo    <federico.ridolfo@polito.it>
+ *    - Eugenio Menegatti     <m.eu@libero.it>
+ *    - Stefano Cau
+ *    - Giuliano Emma
+ *    - Stefano Oldrini
  * 
  *  Fenice is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -41,123 +41,113 @@
 #include <fenice/multicast.h>
 #include <fenice/fnc_log.h>
 
-int send_setup_reply(RTSP_buffer * rtsp, RTSP_session * session, RTP_session * rtp_s)
+int send_setup_reply(RTSP_buffer * rtsp, RTSP_session * session,
+                     RTP_session * rtp_s)
 {
-	char r[1024];
-	int w_pos = 0;
-//	char temp[30];
-//	char ttl[4];
-	/* build a reply message */
+    char r[1024];
+    int w_pos = 0;
 
-	snprintf(r, sizeof(r),
-		"%s %d %s" RTSP_EL "CSeq: %d" RTSP_EL "Server: %s/%s" RTSP_EL,
-		RTSP_VER, 200, get_stat(200), rtsp->rtsp_cseq, PACKAGE,
-		VERSION);
-	add_time_stamp(r, 0);
-	w_pos = strlen(r);
-	/*
-	   strcat(r, "Session: ");
-	   sprintf(temp, "%d", session->session_id);
-	   strcat(r, temp);
-	   strcat(r, RTSP_EL);
-	 */
-	w_pos += snprintf(r + w_pos, sizeof(r) - w_pos, "Session: %d" RTSP_EL, session->session_id);
+    snprintf(r, sizeof(r),
+        "%s %d %s" RTSP_EL "CSeq: %d" RTSP_EL "Server: %s/%s" RTSP_EL,
+        RTSP_VER, 200, get_stat(200), rtsp->rtsp_cseq, PACKAGE,
+        VERSION);
+    add_time_stamp(r, 0);
+    w_pos = strlen(r);
+    w_pos += snprintf(r + w_pos, sizeof(r) - w_pos, "Session: %d" RTSP_EL,
+                      session->session_id);
 
-	w_pos += snprintf(r + w_pos, sizeof(r) - w_pos, "Transport: ");
-	switch (Sock_type(rtp_s->transport.rtp_sock)) {
-	case UDP:
+    w_pos += snprintf(r + w_pos, sizeof(r) - w_pos, "Transport: ");
+    switch (Sock_type(rtp_s->transport.rtp_sock)) {
+    case UDP:
 #if 0
 //Temporary disable of multicast code for netembro
-		// if (!(descr->flags & SD_FL_MULTICAST)) {
-		if (rtp_s->transport.u.udp.is_multicast) {
-			/*
-			   strcat(r, "Transport: RTP/AVP;multicast;");
-			   sprintf(temp, "destination=%s;", descr->multicast);
-			   strcat(r, temp);
-			   strcat(r, "port=");
-			 */
-			w_pos +=
-			    sprintf(r + w_pos,
-				    "RTP/AVP;multicast;ttl=%d;destination=%s;port=",
-				    (int) DEFAULT_TTL, descr->multicast);
-		} else {
+        // if (!(descr->flags & SD_FL_MULTICAST)) {
+        if (rtp_s->transport.u.udp.is_multicast) {
+            /*
+               strcat(r, "Transport: RTP/AVP;multicast;");
+               sprintf(temp, "destination=%s;", descr->multicast);
+               strcat(r, temp);
+               strcat(r, "port=");
+             */
+            w_pos +=
+                sprintf(r + w_pos,
+                    "RTP/AVP;multicast;ttl=%d;destination=%s;port=",
+                    (int) DEFAULT_TTL, descr->multicast);
+        } else {
 #endif //Temporary disable of multicast code for netembro
-			/*
-			   strcat(r, "Transport: RTP/AVP;unicast;client_port=");
-			   sprintf(temp, "%d", rtp_s->transport.u.udp.cli_ports.RTP);
-			   strcat(r, temp);
-			   strcat(r, "-");
-			   sprintf(temp, "%d", rtp_s->transport.u.udp.cli_ports.RTCP);
-			   strcat(r, temp);
+            /*
+               strcat(r, "Transport: RTP/AVP;unicast;client_port=");
+               sprintf(temp, "%d", rtp_s->transport.u.udp.cli_ports.RTP);
+               strcat(r, temp);
+               strcat(r, "-");
+               sprintf(temp, "%d", rtp_s->transport.u.udp.cli_ports.RTCP);
+               strcat(r, temp);
 
-			   sprintf(temp, ";source=%s", get_address());
-			   strcat(r, temp);
+               sprintf(temp, ";source=%s", get_address());
+               strcat(r, temp);
 
-			   strcat(r, ";server_port=");
-			 */
-			w_pos +=
-			    snprintf(r + w_pos, sizeof(r) - w_pos,
-				    "RTP/AVP;unicast;client_port=%d-%d;source=%s;server_port=",
-				    get_remote_port(rtp_s->transport.rtp_sock),
-				    get_remote_port(rtp_s->transport.rtcp_sock),
-				    get_local_host(rtsp->sock));
+               strcat(r, ";server_port=");
+             */
+            w_pos +=
+                snprintf(r + w_pos, sizeof(r) - w_pos,
+                    "RTP/AVP;unicast;client_port=%d-%d;source=%s;server_port=",
+                    get_remote_port(rtp_s->transport.rtp_sock),
+                    get_remote_port(rtp_s->transport.rtcp_sock),
+                    get_local_host(rtsp->sock));
 #if 0
-		}
+        }
 #endif
-		/*
-		   sprintf(temp, "%d", rtp_s->transport.u.udp.ser_ports.RTP);
-		   strcat(r, temp);
-		   strcat(r, "-");
-		   sprintf(temp, "%d", rtp_s->transport.u.udp.ser_ports.RTCP);
-		   strcat(r, temp);
-		 */
-		w_pos +=
-		    snprintf(r + w_pos, sizeof(r) - w_pos, "%d-%d",
-			    get_local_port(rtp_s->transport.rtp_sock),
-			    get_local_port(rtp_s->transport.rtcp_sock));
+        /*
+           sprintf(temp, "%d", rtp_s->transport.u.udp.ser_ports.RTP);
+           strcat(r, temp);
+           strcat(r, "-");
+           sprintf(temp, "%d", rtp_s->transport.u.udp.ser_ports.RTCP);
+           strcat(r, temp);
+         */
+        w_pos +=
+            snprintf(r + w_pos, sizeof(r) - w_pos, "%d-%d",
+                get_local_port(rtp_s->transport.rtp_sock),
+                get_local_port(rtp_s->transport.rtcp_sock));
 
 #if 0
-		// if ((descr->flags & SD_FL_MULTICAST)) {
-		if (rtp_s->transport.u.udp.is_multicast) {
-			/*
-			   strcat(r,";ttl=");
-			   sprintf(ttl,"%d",(int)DEFAULT_TTL);
-			   strcat(r,ttl);
-			 */
-			w_pos +=
-			    sprintf(r + w_pos, ";ttl=%d", (int) DEFAULT_TTL);
-		}
+        // if ((descr->flags & SD_FL_MULTICAST)) {
+        if (rtp_s->transport.u.udp.is_multicast) {
+            /*
+               strcat(r,";ttl=");
+               sprintf(ttl,"%d",(int)DEFAULT_TTL);
+               strcat(r,ttl);
+             */
+            w_pos +=
+                sprintf(r + w_pos, ";ttl=%d", (int) DEFAULT_TTL);
+        }
 #endif
-		break;
-	case LOCAL:
-		if (Sock_type(rtsp->sock) == TCP) {
-			w_pos += snprintf(r + w_pos, sizeof(r) - w_pos,
-				 "RTP/AVP/TCP;interleaved=%d-%d",
-				 rtp_s->transport.rtp_ch,
-				 rtp_s->transport.rtcp_ch);
-		}
-		else if (Sock_type(rtsp->sock) == SCTP) {
-			w_pos += snprintf(r + w_pos, sizeof(r) - w_pos,
-				 "RTP/AVP/SCTP;server_streams=%d-%d",
-				 rtp_s->transport.rtp_ch,
-				 rtp_s->transport.rtcp_ch);
-		}
-		break;
-	default:
-		break;
-	}
-	/*
-	   //sprintf(temp, ";ssrc=%u", session->rtp_session->ssrc);
-	   sprintf(temp, ";ssrc=%u", rtp_s->ssrc); // xxx
-	   strcat(r, temp);
-	 */
-	w_pos += snprintf(r + w_pos, sizeof(r) - w_pos, ";ssrc=%u", rtp_s->ssrc);
-	// strcat(r, "\r\n\r\n");
-	strcat(r, RTSP_EL RTSP_EL);
+        break;
+    case LOCAL:
+        if (Sock_type(rtsp->sock) == TCP) {
+            w_pos += snprintf(r + w_pos, sizeof(r) - w_pos,
+                 "RTP/AVP/TCP;interleaved=%d-%d",
+                 rtp_s->transport.rtp_ch,
+                 rtp_s->transport.rtcp_ch);
+        }
+        else if (Sock_type(rtsp->sock) == SCTP) {
+            w_pos += snprintf(r + w_pos, sizeof(r) - w_pos,
+                 "RTP/AVP/SCTP;server_streams=%d-%d",
+                 rtp_s->transport.rtp_ch,
+                 rtp_s->transport.rtcp_ch);
+        }
+        break;
+    default:
+        break;
+    }
+    rtp_s->ssrc=0;
+    snprintf(r + w_pos, sizeof(r) - w_pos, ";ssrc=%u", rtp_s->ssrc);
 
-	bwrite(r, (unsigned short) strlen(r), rtsp);
+    strcat(r, RTSP_EL RTSP_EL);
 
-	fnc_log(FNC_LOG_CLIENT, "200 - %s ", r_selected_track(rtp_s->track_selector)->info->name);
+    bwrite(r, (unsigned short) strlen(r), rtsp);
 
-	return ERR_NOERROR;
+    fnc_log(FNC_LOG_CLIENT, "200 - %s ",
+            r_selected_track(rtp_s->track_selector)->info->name);
+
+    return ERR_NOERROR;
 }
