96945c2e975e4c85620a7951ec6fbf454340b1c0
Luca Barbato <lu_zero@gentoo.org>
2007-07-04 14:52:26 +0200

    Cosmetics

--- mediathread/cache.c
+++ mediathread/cache.c
@@ -6,11 +6,11 @@
  *  Feng -- Standard Streaming Server
  *
  *  Copyright (C) 2007 by
- *  	
- *	- Giampaolo Mancini	<giampaolo.mancini@polito.it>
- *	- Francesco Varano	<francesco.varano@polito.it>
- *	- Marco Penno		<marco.penno@polito.it>
- *	- Federico Ridolfo	<federico.ridolfo@polito.it>
+ *      
+ *    - Giampaolo Mancini    <giampaolo.mancini@polito.it>
+ *    - Francesco Varano    <francesco.varano@polito.it>
+ *    - Marco Penno        <marco.penno@polito.it>
+ *    - Federico Ridolfo    <federico.ridolfo@polito.it>
  * 
  *  Feng is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -45,46 +45,46 @@ static int read_from_device(int fd, void *buf, size_t nbytes);/*not implemented 
 // shawill: should we receive cache size as parameter?
 Cache *create_cache(stream_type type)
 {
-	uint32 size;
-	Cache *c;
-	
-	if ( !(c=(Cache *)malloc(sizeof(Cache))) )
-		return NULL;
+    uint32 size;
+    Cache *c;
+    
+    if ( !(c=(Cache *)malloc(sizeof(Cache))) )
+        return NULL;
 
-	switch(type) {
-		case st_file:
-			size=CACHE_FILE_SIZE;
-			c->read_data=read;
-			break;
-		case st_net:
-			size=CACHE_NET_SIZE;
-			c->read_data=read_from_net;
-			break;
-		case st_pipe:
-			size=CACHE_PIPE_SIZE;
-			c->read_data=read;
-			break;
-		case st_device:
-			size=CACHE_DEVICE_SIZE;
-			c->read_data=read_from_device;
-			break;
-		default:
-			size=CACHE_DEFAULT_SIZE;
-			c->read_data=read;
-			break;
-	}
-	if ( !(c->cache=(uint8 *)malloc(size)) ) {
-		free(c);
-		return NULL;
-	}
-	if(c->cache==NULL){
-		free(c);
-		return NULL;
-	}
-	c->max_cache_size=size;
-	c->bytes_left=0;
+    switch(type) {
+        case st_file:
+            size=CACHE_FILE_SIZE;
+            c->read_data=read;
+            break;
+        case st_net:
+            size=CACHE_NET_SIZE;
+            c->read_data=read_from_net;
+            break;
+        case st_pipe:
+            size=CACHE_PIPE_SIZE;
+            c->read_data=read;
+            break;
+        case st_device:
+            size=CACHE_DEVICE_SIZE;
+            c->read_data=read_from_device;
+            break;
+        default:
+            size=CACHE_DEFAULT_SIZE;
+            c->read_data=read;
+            break;
+    }
+    if ( !(c->cache=(uint8 *)malloc(size)) ) {
+        free(c);
+        return NULL;
+    }
+    if(c->cache==NULL){
+        free(c);
+        return NULL;
+    }
+    c->max_cache_size=size;
+    c->bytes_left=0;
 
-	return c;
+    return c;
 }
 
 
@@ -93,62 +93,62 @@ Cache *create_cache(stream_type type)
  * */
 static uint32 read_internal_c(uint32 nbytes, uint8 *buf, Cache *c, int fd)
 {
-	uint32 bytes;
+    uint32 bytes;
 
-	if(!nbytes)
-		return 0;
-		
-	if(!c->bytes_left) {
-		c->bytes_left=c->cache_size=c->read_data(fd,c->cache,c->max_cache_size);/*can be: read, read_from_net, read_from_device*/
-		if(!c->cache_size) /*EOF*/
-			return 0;
-	}
-	bytes=min(nbytes,c->bytes_left);
-	if (buf)
-		memcpy(buf, &c->cache[c->cache_size - c->bytes_left], bytes);
-	c->bytes_left-=bytes;
-	
-	return bytes + read_internal_c(nbytes-bytes, buf+bytes, c, fd);
+    if(!nbytes)
+        return 0;
+        
+    if(!c->bytes_left) {
+        c->bytes_left=c->cache_size=c->read_data(fd,c->cache,c->max_cache_size);/*can be: read, read_from_net, read_from_device*/
+        if(!c->cache_size) /*EOF*/
+            return 0;
+    }
+    bytes=min(nbytes,c->bytes_left);
+    if (buf)
+        memcpy(buf, &c->cache[c->cache_size - c->bytes_left], bytes);
+    c->bytes_left-=bytes;
+    
+    return bytes + read_internal_c(nbytes-bytes, buf+bytes, c, fd);
 }
 
 
 /*Interface to Cache*/
 int read_c(uint32 nbytes, uint8 *buf, Cache **c, int fd, stream_type type)
 {
-	int bytes_read;
+    int bytes_read;
 //FIXME make sure it is thread safe!
-	if( !*c && !(*c=create_cache(type))) { // shawill: should we do it here?
-		fnc_log(FNC_LOG_FATAL, "Could not create cache for input stream\n");
-		return ERR_FATAL;
-	}
+    if( !*c && !(*c=create_cache(type))) { // shawill: should we do it here?
+        fnc_log(FNC_LOG_FATAL, "Could not create cache for input stream\n");
+        return ERR_FATAL;
+    }
 
-	bytes_read=read_internal_c(nbytes, buf, *c, fd);
+    bytes_read=read_internal_c(nbytes, buf, *c, fd);
 
-	return bytes_read ? bytes_read : ERR_EOF;
+    return bytes_read ? bytes_read : ERR_EOF;
 }
 
 void flush_cache(Cache *c)
 {
-	if(c!=NULL)
-		c->bytes_left=0;
+    if(c!=NULL)
+        c->bytes_left=0;
 } 
 
 void free_cache(Cache *c)
 {
-	if(c!=NULL){
-		free(c->cache);
-		c->cache=NULL;
-		free(c);
-		c=NULL;
-	}
+    if(c!=NULL){
+        free(c->cache);
+        c->cache=NULL;
+        free(c);
+        c=NULL;
+    }
 }
 
 static int read_from_net(int fd, void *buf, size_t nbytes)
 {
-	return -1;
+    return -1;
 }
 static int read_from_device(int fd, void *buf, size_t nbytes)
 {
-	return -1;
+    return -1;
 }
 
