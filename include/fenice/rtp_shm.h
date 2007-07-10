986d4153bc2bd0ea0eee3758c6d7cdb67555a822
Luca Barbato <lu_zero@gentoo.org>
2007-07-10 02:24:48 +0200

    Updated copyright notices

--- include/fenice/rtp_shm.h
+++ include/fenice/rtp_shm.h
@@ -1,9 +1,9 @@
 /* * 
  *  $Id: rtp_shm.h 387 2006-09-26 15:12:00Z shawill $
  *  
- *  This file is part of Fenice
+ *  This file is part of Feng
  *
- *  Fenice -- Open Media Server
+ *  Feng -- Open Media Server
  *
  *  Copyright (C) 2007 by
  *  	
@@ -16,19 +16,36 @@
  *	- Giuliano Emma
  *	- Stefano Oldrini
  * 
- *  Fenice is free software; you can redistribute it and/or modify
+ *  Feng is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
- *  Fenice is distributed in the hope that it will be useful,
+ *  Feng is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Fenice; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  along with Feng; if not, write to the Free Software
+ *  This file is part of Feng
+ * 
+ * Copyright (C) 2007 by LScube team <team@streaming.polito.it> 
+ * See AUTHORS for more details 
+ *  
+ * Feng is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ * 
+ * Feng is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Feng; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *  
  * */
 
--- src/prefs.c
+++ src/prefs.c
@@ -28,7 +28,24 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with Feng; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  This file is part of Feng
+ * 
+ * Copyright (C) 2007 by LScube team <team@streaming.polito.it> 
+ * See AUTHORS for more details 
+ *  
+ * Feng is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ * 
+ * Feng is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Feng; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *  
  * */
 
