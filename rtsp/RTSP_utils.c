cfa119da66e489ff3012360d5e3ec54fa9adbe76
Luca Barbato <lu_zero@gentoo.org>
2007-07-16 10:12:37 +0200

    License header cleanup

--- rtsp/RTSP_utils.c
+++ rtsp/RTSP_utils.c
@@ -1,22 +1,6 @@
-/*
- *  $Id: $
- *
- *  Copyright (C) 2007 by team@streaming.polito.it
- *
- *  Feng is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  libnms is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with libnms; if not, write to the Free Software
+/* *
  *  This file is part of Feng
- * 
+ *
  * Copyright (C) 2007 by LScube team <team@streaming.polito.it> 
  * See AUTHORS for more details 
  *  
