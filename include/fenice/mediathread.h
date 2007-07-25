468effae0fc938da2979d48238f7834fc9e2bf49
Austin Foxley <austinf@cetoncorp.com>
2007-07-25 19:55:21 +0200

    fix some stray tabs

--- include/fenice/mediathread.h
+++ include/fenice/mediathread.h
@@ -36,27 +36,27 @@
 
 /*
 typedef struct __MT_RESOURCE_ITEM {
-	Resource *resource;
-	pthread_mutex_t *mutex;
-	struct __MT_RESOURCE_ITEM *next;
+    Resource *resource;
+    pthread_mutex_t *mutex;
+    struct __MT_RESOURCE_ITEM *next;
 } mt_resource_item;
 */
 
 typedef enum __MT_EVENT_ID {
-	MT_EV_NOP,		//Fake event do nothing. ARGS= NULL;
-	MT_EV_BUFFER_LOW,	//Buffer needs to be filled. ARGS= Track*
-	MT_EV_DATA_EOF,		//Track has no more data. ARGS= Track*
-	MT_EV_DATA_BOUND	//Track reached request bound. ARGS= Track*
+    MT_EV_NOP,        //Fake event do nothing. ARGS= NULL;
+    MT_EV_BUFFER_LOW,    //Buffer needs to be filled. ARGS= Track*
+    MT_EV_DATA_EOF,        //Track has no more data. ARGS= Track*
+    MT_EV_DATA_BOUND    //Track reached request bound. ARGS= Track*
 } mt_event_id;
 
 typedef struct __MT_EVENT_ITEM {
-	mt_event_id id;
-	void **args;		//! args[0] = sender
+    mt_event_id id;
+    void **args;        //! args[0] = sender
 } mt_event_item;
 
 typedef struct __MT_EXCL_INS {
-	InputStream *i_stream;
-	struct __MT_EXCL_INS *next;
+    InputStream *i_stream;
+    struct __MT_EXCL_INS *next;
 } mt_excl_ins;
 
 void *mediathread(void *arg);
