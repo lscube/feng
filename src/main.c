129ba70b08daee137ae56d585a5bc0cfcd6e169a
Luca Barbato <lu_zero@gentoo.org>
2008-02-04 17:06:21 +0100

    move args parsing in main

--- src/main.c
+++ src/main.c
@@ -34,10 +34,10 @@
 #include <fenice/eventloop.h>
 #include <fenice/prefs.h>
 #include <fenice/utils.h>
-#include <fenice/command_environment.h>
 #include <fenice/mediathread.h>
 #include <glib.h>
 #include <glib/gprintf.h>
+#include <getopt.h>
 
 #include <pthread.h>
 
@@ -180,6 +180,87 @@ static int feng_start_mt(feng *srv)
     }
 
     pthread_create(&srv->mth, NULL, mediathread, NULL);
+    return 0;
+}
+static void usage(char *name)
+{
+    fprintf(stdout,
+        "%s [options] \n"
+        "--help\t\t| -h | -? \tshow this message\n"
+        "--quiet\t\t| -q \tshow as little output as possible\n"
+        "--config\t| -f <config> \tspecify configuration file\n"
+        "--verbose\t| -v \t\toutput to standar error (debug)\n"
+        "--version\t| -V \t\tprint version and exit\n"
+        "--syslog\t| -s \t\tuse syslog facility\n", name);
+    return;
+}
+
+static int command_environment(feng *srv, int argc, char **argv)
+{
+    static const char short_options[] = "f:vVsq";
+    //"m:a:f:n:b:z:T:B:q:o:S:I:r:M:4:2:Q:X:D:g:G:v:V:F:N:tpdsZHOcCPK:E:R:";
+
+    int n;
+    int config_file = 0;
+    int quiet = 0;
+    int view_log = FNC_LOG_FILE;
+    char *progname = g_path_get_basename(argv[0]);
+    fnc_log_t fn;
+
+    static struct option long_options[] = {
+        {"config",   1, 0, 'f'},
+        {"quiet",    0, 0, 'q'},
+        {"verbose",  0, 0, 'v'},
+        {"version",  0, 0, 'V'},
+        {"syslog",   0, 0, 's'},
+        {"help",     0, 0, '?'},
+        {0,          0, 0,  0 }
+    };
+
+    while ((n = getopt_long(argc, argv, short_options, long_options, NULL))
+                != -1)
+    {
+        switch (n) {
+        case 0:    /* Flag setting handled by getopt-long */
+            break;
+        case 'f':
+            prefs_init(srv, optarg);
+            config_file = 1;
+            break;
+        case 'q':
+            quiet = 1;
+            break;
+        case 'v':
+            view_log = FNC_LOG_OUT;
+            break;
+        case 's':
+            view_log = FNC_LOG_SYS;
+            break;
+        case '?':
+            fncheader();
+            usage(progname);
+            g_free(progname);
+            return 1;
+            break;
+case 'V':
+            fncheader();
+            g_free(progname);
+            return 1;
+            break;
+        default:
+            break;
+        }
+    }
+
+    if (!quiet) fncheader();
+
+    if (!config_file) prefs_init(srv, NULL);
+
+    fn = fnc_log_init(prefs_get_log(), view_log, progname);
+
+    Sock_init(fn);
+    bp_log_init(fn);
+
     return 0;
 }
 
