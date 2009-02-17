94b47463b93e206d356db4f1fd5e86c373a76f7c
Luca Barbato <lu_zero@gentoo.org>
2009-02-17 16:56:54 +0100

    License and uri references changes
    live.polito.it -> lscube.org
    GPL -> LGPL
    team@streaming.polito.it -> team@lscube.org

--- src/network/rtcp.h
+++ src/network/rtcp.h
@@ -1,23 +1,23 @@
 /* * 
- *  This file is part of Feng
+ * This file is part of Feng
+ *
+ * Copyright (C) 2009 by LScube team <team@lscube.org>
+ * See AUTHORS for more details
  * 
- * Copyright (C) 2008 by LScube team <team@streaming.polito.it>
- * See AUTHORS for more details 
- *  
- * Feng is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- * 
- * Feng is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
- * General Public License for more details. 
- * 
- * You should have received a copy of the GNU General Public License
- * along with Feng; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- *  
+ * bufferpool is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * bufferpool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with bufferpool; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA 
+ *
  * */
 
 #ifndef FN_RTCP_H
