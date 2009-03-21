5bfa3823fe52aebf473fb79d66d04b61dce03f23
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2009-03-21 09:02:39 +0100

    Whitespace cleanup.

--- src/mediathread/parser/amr.c
+++ src/mediathread/parser/amr.c
@@ -1,9 +1,9 @@
-/* * 
+/* *
  * This file is part of Feng
  *
  * Copyright (C) 2009 by LScube team <team@lscube.org>
  * See AUTHORS for more details
- * 
+ *
  * feng is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -16,7 +16,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with feng; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * */
 
@@ -82,10 +82,10 @@ static int amr_init(MediaProperties *properties, void **private_data)
 /* AMR Payload Header (RFC3267)
     0  1 2 3 4 5   6 7
    +-+-+-+-+-+-+
-   | CMR  |   RR     |  
+   | CMR  |   RR     |
    +-+-+-+-+-+-+
    +-+-+-+-+-+-+
-   |F|  FT    |Q  PP |  
+   |F|  FT    |Q  PP |
    +-+-+-+-+-+-+
 */
 
@@ -155,14 +155,14 @@ static int amr_parse(void *track, uint8_t *data, long len, uint8_t *extradata,
     payload = DEFAULT_MTU - header_len;
     fnc_log(FNC_LOG_DEBUG, "AMR:data len: %d, frame body len: %d", len,body_len);
 
-    if (len > payload) {    
+    if (len > payload) {
         /*fill toc to payload packet*/
         for(i = 1; i < body_num; i++)
         {
             memcpy(packet+ i, &tocv, 1);
         }
         memcpy(packet+body_num, &end_tocv, 1);
-        
+
         /*fill the frame content*/
         while (len > payload) {
             memcpy(packet + header_len, data + off, payload);
@@ -188,7 +188,7 @@ static int amr_parse(void *track, uint8_t *data, long len, uint8_t *extradata,
         memcpy(packet+ i, &tocv, 1);
     }
     memcpy(packet+body_num, &end_tocv, 1);
-    
+
     /*fill the frame content*/
     memcpy(packet + header_len, data + off, len);
     if (bp_write(tr->buffer, 0, tr->properties->mtime, 0, 0,
