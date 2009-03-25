lscube/feng

commit f7041d50039939ba1ec86483223aa5bfd0838d9b

Simplify handling of interleaved transports.
Move the ev_io_listen object inside RTSP_interleaved; replace the
singly-linked list of RTSP_interleaved objects with a pair of inline
objects.

src/eventloop.c
@@ -117,7 +117,7 @@ find_sctp_interleaved(gconstpointer value, gconstpointer target)
 
 #endif
 
-void eventloop_local_callbacks(RTSP_buffer *rtsp, RTSP_interleaved *intlvd)
+void eventloop_local_callbacks(RTSP_buffer *rtsp)
 {
     void (*cb)(EV_P_ struct ev_io *w, int revents);
     int i;
@@ -138,11 +138,11 @@ void eventloop_local_callbacks(RTSP_buffer *rtsp, RTSP_interleaved *intlvd)
 
     /* Let's hope this is unrolled by GCC, shall we? */
     for (i = 0; i < 2; i++) {
-        Sock *sock = intlvd[i].local;
+        Sock *sock = rtsp->interleaved[i].local;
         sock->data = rtsp;
-        rtsp->ev_io_listen[i].data = intlvd + i;
-        ev_io_init(&rtsp->ev_io_listen[i], cb, Sock_fd(sock), EV_READ);
-        ev_io_start(rtsp->srv->loop, &rtsp->ev_io_listen[i]);
+        rtsp->interleaved[i].ev_io_listen.data = &rtsp->interleaved[i];
+        ev_io_init(&rtsp->interleaved[i].ev_io_listen, cb, Sock_fd(sock), EV_READ);
+        ev_io_start(rtsp->srv->loop, &rtsp->interleaved[i].ev_io_listen);
     }
 }
 

src/network/rtsp.h
@@ -77,6 +77,7 @@ typedef enum {
 typedef struct RTSP_interleaved {
     Sock *local;
     int channel;
+    ev_io ev_io_listen;
 } RTSP_interleaved;
 
 typedef struct RTSP_session {
@@ -96,15 +97,10 @@ typedef struct RTSP_buffer {
     char in_buffer[RTSP_BUFFERSIZE];
     size_t in_size;
     GAsyncQueue *out_queue;
-    GSList *interleaved;
+    RTSP_interleaved interleaved[2];
 
     ev_io ev_io_read;
     ev_io ev_io_write;
-
-    /**
-     * Pair of listener instances used for interleaved and SCTP.
-     */
-    ev_io ev_io_listen[2];
 
     // Run-Time
     RTSP_session *session;

src/network/rtsp_method_setup.c
@@ -39,7 +39,7 @@
 #include <fenice/schedule.h>
 
 // XXX move in an header
-void eventloop_local_callbacks(RTSP_buffer *rtsp, RTSP_interleaved *intlvd);
+void eventloop_local_callbacks(RTSP_buffer *rtsp);
 
 /**
  * Splits the path of a requested media finding the trackname and the removing
@@ -115,7 +115,6 @@ static gboolean
 interleaved_transport(RTSP_buffer *rtsp, RTP_transport *transport,
                       int rtp_ch, int rtcp_ch)
 {
-    RTSP_interleaved *intlvd;
     Sock *sock_pair[2];
 
     // RTP local sockpair
@@ -125,31 +124,26 @@ interleaved_transport(RTSP_buffer *rtsp, RTP_transport *transport,
         return false;
     }
 
-    intlvd = g_new0(RTSP_interleaved, 2);
-
     transport->rtp_sock = sock_pair[0];
-    intlvd[0].local = sock_pair[1];
+    rtsp->interleaved[0].local = sock_pair[1];
 
     // RTCP local sockpair
     if ( Sock_socketpair(sock_pair) < 0) {
         fnc_log(FNC_LOG_ERR,
                 "Cannot create AF_LOCAL socketpair for rtcp\n");
         Sock_close(transport->rtp_sock);
-        Sock_close(intlvd[0].local);
-        g_free(intlvd);
+        Sock_close(rtsp->interleaved[0].local);
         return false;
     }
 
     transport->rtcp_sock = sock_pair[0];
-    intlvd[1].local = sock_pair[1];
+    rtsp->interleaved[1].local = sock_pair[1];
 
     // copy stream number in rtp_transport struct
-    transport->rtp_ch = intlvd[0].channel = rtp_ch;
-    transport->rtcp_ch = intlvd[1].channel = rtcp_ch;
+    transport->rtp_ch = rtsp->interleaved[0].channel = rtp_ch;
+    transport->rtcp_ch = rtsp->interleaved[1].channel = rtcp_ch;
 
-    rtsp->interleaved = g_slist_prepend(rtsp->interleaved, intlvd);
-
-    eventloop_local_callbacks(rtsp, intlvd);
+    eventloop_local_callbacks(rtsp);
 
     return true;
 }

src/network/rtsp_utils.c
@@ -109,15 +109,6 @@ RTSP_buffer *rtsp_client_new(feng *srv, Sock *client_sock)
     return new;
 }
 
-static void interleaved_close_fds(gpointer element, gpointer user_data)
-{
-    RTSP_interleaved *intlvd = (RTSP_interleaved *)element;
-
-    Sock_close(intlvd[0].local);
-    Sock_close(intlvd[1].local);
-    g_free(intlvd);
-}
-
 /**
  * Destroy and free resources for an RTSP client structure
  *
@@ -128,6 +119,7 @@ static void interleaved_close_fds(gpointer element, gpointer user_data)
 void rtsp_client_destroy(RTSP_buffer *rtsp)
 {
   GString *outbuf = NULL;
+  int i;
 
   if (rtsp->session != NULL) {
 
@@ -142,13 +134,10 @@ void rtsp_client_destroy(RTSP_buffer *rtsp)
     rtsp->session = NULL;
   }
 
-  // close local fds
-  g_slist_foreach(rtsp->interleaved, interleaved_close_fds, NULL);
-
-  g_slist_free(rtsp->interleaved);
-
-  ev_io_stop(rtsp->srv->loop, &rtsp->ev_io_listen[0]);
-  ev_io_stop(rtsp->srv->loop, &rtsp->ev_io_listen[1]);
+  for(i = 0; i < 2; i++) {
+      Sock_close(rtsp->interleaved[i].local);
+      ev_io_stop(rtsp->srv->loop, &rtsp->interleaved[i].ev_io_listen);
+  }
 
   // Remove the output queue
   g_async_queue_lock(rtsp->out_queue);

commit c518ec3f60c2842fbc160904030b5e0328467af7

Restore the singly-linked list for RTSP_interleaved.
Commits e53ac909d03c6134efd15413457e7308486bbc1d and
f7041d50039939ba1ec86483223aa5bfd0838d9b broke interleaved support since
they assumed a single interleaved stream per session, but we usually have
more than that.

The new code is a mix between the original and the updated one, and
something new at the same time: the ev_io object is inlined in
RTSP_interleaved_channel, and RTSP_interleaved is formed by two
RTSP_interleaved_channel inline objects called rtp and rtcp, to make them
self-explanatory.

src/eventloop.c
@@ -64,7 +64,7 @@ void interleaved_read_tcp_cb(struct ev_loop *loop, ev_io *w, int revents)
     GString *str;
     uint16_t ne_n;
     char buffer[RTSP_BUFFERSIZE + 1];
-    RTSP_interleaved *intlvd = w->data;
+    RTSP_interleaved_channel *intlvd = w->data;
     RTSP_buffer *rtsp = intlvd->local->data;
     int n;
 
@@ -90,7 +90,7 @@ void interleaved_read_tcp_cb(struct ev_loop *loop, ev_io *w, int revents)
 void interleaved_read_sctp_cb(struct ev_loop *loop, ev_io *w, int revents)
 {
     char buffer[RTSP_BUFFERSIZE + 1];
-    RTSP_interleaved *intlvd = w->data;
+    RTSP_interleaved_channel *intlvd = w->data;
     RTSP_buffer *rtsp = intlvd->local->data;
     struct sctp_sndrcvinfo sctp_info;
     int n;
@@ -110,7 +110,7 @@ find_sctp_interleaved(gconstpointer value, gconstpointer target)
 {
     RTSP_interleaved *i = (RTSP_interleaved *)value;
     gint m = GPOINTER_TO_INT(target);
-    return (i[1].channel == m);
+    return (i->rtcp.channel == m);
 }
 
 #endif
@@ -149,7 +149,7 @@ static void rtsp_read_cb(struct ev_loop *loop, ev_io *w, int revents)
             g_slist_find_custom(rtsp->interleaved, GINT_TO_POINTER(m),
                                 find_sctp_interleaved)->data;
         if (intlvd) {
-            Sock_write(intlvd->local, buffer, n, NULL, 0);
+            Sock_write(intlvd->rtcp.local, buffer, n, NULL, 0);
         } else {
             fnc_log(FNC_LOG_DEBUG, "Unknown rtcp stream %d ", m);
         }

src/network/rtsp.h
@@ -74,10 +74,30 @@ typedef enum {
 #define RTSP_EL "\r\n"
 #define RTSP_RTP_AVP "RTP/AVP"
 
-typedef struct RTSP_interleaved {
+/**
+ * @brief Represents a single channel of an interleaved RTSP
+ *        connection.
+ */
+typedef struct {
+    /**
+     * Local socket to forward the received content to.
+     */
     Sock *local;
+
+    /**
+     * Channel number used for receiving
+     */
     int channel;
+
+    /**
+     * libev handler for the read callback
+     */
     ev_io ev_io_listen;
+} RTSP_interleaved_channel;
+
+typedef struct RTSP_interleaved {
+    RTSP_interleaved_channel rtp;
+    RTSP_interleaved_channel rtcp;
 } RTSP_interleaved;
 
 typedef struct RTSP_session {
@@ -107,20 +127,20 @@ typedef struct RTSP_buffer {
     RTSP_session *session;
     feng *srv;
 
+    /**
+     * @brief Interleaved setup data
+     *
+     * Singly-linked list of @ref RTSP_interleaved instances, one per
+     * RTP session linked to the RTSP session.
+     */
+    GSList *interleaved;
+
     //Events
     ev_async *ev_sig_disconnect;
     ev_timer *ev_timeout;
 
     ev_io ev_io_read;
     ev_io ev_io_write;
-
-    /**
-     * @brief Pair of interleaved data
-     *
-     * This array contains the pair of, respectively, rtp and rtcp
-     * ports emulator for interleaved connections.
-     */
-    RTSP_interleaved interleaved[2];
 } RTSP_buffer;
 
 /**

src/network/rtsp_method_setup.c
@@ -108,7 +108,7 @@ static RTSP_ResponseCode unicast_transport(RTSP_buffer *rtsp,
  * interleaved transport
  */
 
-static void setup_interleaved_callbacks(RTSP_buffer *rtsp)
+static void setup_interleaved_callbacks(RTSP_buffer *rtsp, RTSP_interleaved *intlvd)
 {
     void (*cb)(EV_P_ struct ev_io *w, int revents);
     int i;
@@ -127,50 +127,55 @@ static void setup_interleaved_callbacks(RTSP_buffer *rtsp)
 #endif
     }
 
-    /* Let's hope this is unrolled by GCC, shall we? */
-    for (i = 0; i < 2; i++) {
-        Sock *sock = rtsp->interleaved[i].local;
-        sock->data = rtsp;
-        rtsp->interleaved[i].ev_io_listen.data = &rtsp->interleaved[i];
-        ev_io_init(&rtsp->interleaved[i].ev_io_listen, cb, Sock_fd(sock), EV_READ);
-        ev_io_start(rtsp->srv->loop, &rtsp->interleaved[i].ev_io_listen);
-    }
-}
+    intlvd->rtp.local->data = rtsp;
+    intlvd->rtp.ev_io_listen.data = &intlvd->rtp;
+    ev_io_init(&intlvd->rtp.ev_io_listen, cb, Sock_fd(intlvd->rtp.local), EV_READ);
+    ev_io_start(rtsp->srv->loop, &intlvd->rtp.ev_io_listen);
 
+    intlvd->rtcp.local->data = rtsp;
+    intlvd->rtcp.ev_io_listen.data = &intlvd->rtcp;
+    ev_io_init(&intlvd->rtcp.ev_io_listen, cb, Sock_fd(intlvd->rtcp.local), EV_READ);
+    ev_io_start(rtsp->srv->loop, &intlvd->rtcp.ev_io_listen);
+}
 
 static gboolean
 interleaved_transport(RTSP_buffer *rtsp, RTP_transport *transport,
                       int rtp_ch, int rtcp_ch)
 {
-    Sock *sock_pair[2];
+    RTSP_interleaved *intlvd = NULL;
+    Sock *sock_pair[2][2];
 
     // RTP local sockpair
-    if ( Sock_socketpair(sock_pair) < 0) {
+    if ( Sock_socketpair(sock_pair[0]) < 0) {
         fnc_log(FNC_LOG_ERR,
                 "Cannot create AF_LOCAL socketpair for rtp\n");
         return false;
     }
 
-    transport->rtp_sock = sock_pair[0];
-    rtsp->interleaved[0].local = sock_pair[1];
-
     // RTCP local sockpair
-    if ( Sock_socketpair(sock_pair) < 0) {
+    if ( Sock_socketpair(sock_pair[1]) < 0) {
         fnc_log(FNC_LOG_ERR,
                 "Cannot create AF_LOCAL socketpair for rtcp\n");
-        Sock_close(transport->rtp_sock);
-        Sock_close(rtsp->interleaved[0].local);
+        Sock_close(sock_pair[0][0]);
+        Sock_close(sock_pair[0][1]);
         return false;
     }
 
-    transport->rtcp_sock = sock_pair[0];
-    rtsp->interleaved[1].local = sock_pair[1];
+    intlvd = g_slice_new0(RTSP_interleaved);
+
+    transport->rtp_sock = sock_pair[0][0];
+    intlvd->rtp.local = sock_pair[0][1];
+
+    transport->rtcp_sock = sock_pair[1][0];
+    intlvd->rtcp.local = sock_pair[1][1];
 
     // copy stream number in rtp_transport struct
-    transport->rtp_ch = rtsp->interleaved[0].channel = rtp_ch;
-    transport->rtcp_ch = rtsp->interleaved[1].channel = rtcp_ch;
+    transport->rtp_ch = intlvd->rtp.channel = rtp_ch;
+    transport->rtcp_ch = intlvd->rtcp.channel = rtcp_ch;
 
-    setup_interleaved_callbacks(rtsp);
+    setup_interleaved_callbacks(rtsp, intlvd);
+
+    rtsp->interleaved = g_slist_prepend(rtsp->interleaved, intlvd);
 
     return true;
 }

src/network/rtsp_state_machine.c
@@ -431,7 +431,7 @@ find_tcp_interleaved(gconstpointer value, gconstpointer target)
   RTSP_interleaved *i = (RTSP_interleaved *)value;
   gint m = GPOINTER_TO_INT(target);
 
-  return (i[1].channel == m);
+  return (i->rtcp.channel == m);
 }
 
 static void rtsp_handle_interleaved(RTSP_buffer *rtsp, int blen, int hlen)
@@ -455,7 +455,7 @@ static void rtsp_handle_interleaved(RTSP_buffer *rtsp, int blen, int hlen)
     fnc_log(FNC_LOG_DEBUG,
             "Interleaved RTCP packet arrived for channel %d (len: %d).\n",
             m, blen);
-    Sock_write(channel->local, &rtsp->input->data[hlen],
+    Sock_write(channel->rtcp.local, &rtsp->input->data[hlen],
                blen, NULL, MSG_DONTWAIT | MSG_EOR);
 }
 

src/network/rtsp_utils.c
@@ -111,6 +111,29 @@ RTSP_buffer *rtsp_client_new(feng *srv, Sock *client_sock)
 }
 
 /**
+ * @brief Free a RTSP_interleaved instance
+ *
+ * @param element Instance to destroy and free
+ * @param user_data The loop to stop libev I/O
+ *
+ * @internal This function should only be called through
+ *           g_slist_foreach() by @ref rtsp_client_destroy.
+ */
+static void rtsp_interleaved_destroy(gpointer element, gpointer user_data)
+{
+    struct ev_loop *loop = (struct ev_loop *)user_data;
+    RTSP_interleaved *intlvd = (RTSP_interleaved *)element;
+
+    Sock_close(intlvd->rtp.local);
+    ev_io_stop(loop, &intlvd->rtp.ev_io_listen);
+
+    Sock_close(intlvd->rtcp.local);
+    ev_io_stop(loop, &intlvd->rtcp.ev_io_listen);
+
+    g_slice_free(RTSP_interleaved, element);
+}
+
+/**
  * Destroy and free resources for an RTSP client structure
  *
  * @param rtsp The client structure to destroy and free
@@ -120,7 +143,6 @@ RTSP_buffer *rtsp_client_new(feng *srv, Sock *client_sock)
 void rtsp_client_destroy(RTSP_buffer *rtsp)
 {
   GString *outbuf = NULL;
-  int i;
 
   if (rtsp->session != NULL) {
 
@@ -135,10 +157,8 @@ void rtsp_client_destroy(RTSP_buffer *rtsp)
     rtsp->session = NULL;
   }
 
-  for(i = 0; i < 2; i++) {
-      Sock_close(rtsp->interleaved[i].local);
-      ev_io_stop(rtsp->srv->loop, &rtsp->interleaved[i].ev_io_listen);
-  }
+  g_slist_foreach(rtsp->interleaved, rtsp_interleaved_destroy, rtsp->srv->loop);
+  g_slist_free(rtsp->interleaved);
 
   // Remove the output queue
   g_async_queue_lock(rtsp->out_queue);