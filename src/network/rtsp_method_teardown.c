69e5826b1669ee9a81fc7107cc1ade99de7a828c
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2009-03-26 01:33:35 +0100

    Remove pretty obvious typo.

--- src/network/rtsp_method_teardown.c
+++ src/network/rtsp_method_teardown.c
@@ -44,7 +44,7 @@ void RTSP_teardown(RTSP_Client * rtsp, RTSP_Request *req)
     Url url;
     RTSP_session *s;
 
-    if ( !rtsp_request_check_url(req) );
+    if ( !rtsp_request_check_url(req) )
         return;
 
     s = rtsp->session;
