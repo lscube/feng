b19aa09e0e8018ff362650a0833fa195be4e92dc
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2009-04-04 02:45:18 +0200

    Fix include to avoid implicit declaration of inet_ntoa.

--- src/mediathread/demuxer/demuxer_sd.c
+++ src/mediathread/demuxer/demuxer_sd.c
@@ -19,10 +19,11 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * */
-#include <string.h>
-#include <strings.h>
-#include <stdio.h>
+
+#include "config.h"
+
 #include <stdbool.h>
+#include <arpa/inet.h>
 
 #include "feng.h"
 #include "feng_utils.h"
