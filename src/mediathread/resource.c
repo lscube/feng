f24476aa2d83aedfc345489056326837346eeb4c
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2009-05-21 16:11:38 +0200

    Replace the r_read function with a r_fill instead.
    The r_fill function takes the consumer to fill up as an argument, and it
    repeats the read until the consumer is filled up with 16 unseen
    frames.
    
    This removes the assumption 1 packet -> 1 frame, and make sures that if
    both the audio and video consumers have to be filled up, it doesn't request
    reading of too many packages at once.

--- src/mediathread/resource.c
+++ src/mediathread/resource.c
@@ -59,8 +59,8 @@ static void r_free_cb(gpointer resource_p, gpointer user_data)
 
     /* Call this first, so that all the reading threads will stop
        before continuing to free resources */
-    if (resource->read_pool)
-        g_thread_pool_free(resource->read_pool, true, true);
+    if (resource->fill_pool)
+        g_thread_pool_free(resource->fill_pool, true, true);
 
     if (resource->lock)
         g_mutex_free(resource->lock);
@@ -80,47 +80,55 @@ static void r_free_cb(gpointer resource_p, gpointer user_data)
 }
 
 /**
- * @brief Callback function to read from a resource
+ * @brief Callback function to fill a track from a resource
  *
- * @param count_p The number of iterations to execute for reading
+ * @param consumer_p The consumer to fill with data
  * @param user_data The resource to read data from
  *
  * This function is used to read more data from the resource into the
- * buffers, to make sure that there is enough data to send to the
+ * track's buffer, to make sure there is enough data to send to the
  * clients.
  *
  * @internal This function is used to initialise @ref
  *           Resource::read_pool
  */
-static void r_read_cb(gpointer count_p, gpointer user_data)
+static void r_fill_cb(gpointer consumer_p, gpointer user_data)
 {
     Resource *resource = (Resource*)user_data;
-    gint count = GPOINTER_TO_INT(count_p);
-    int result;
+    BufferQueue_Consumer *consumer = (BufferQueue_Consumer*)consumer_p;
 
     g_mutex_lock(resource->lock);
 
-    while (
-        (result = resource->demuxer->read_packet(resource)) == RESOURCE_OK &&
-            --count > 0 );
+    while ( (bq_consumer_unseen(consumer) < resource->srv->srvconf.buffered_frames) ) {
+        fprintf(stderr, "calling read_packet for %p[%s] (%u/%d) -> ",
+                resource, resource->info->mrl,
+                bq_consumer_unseen(consumer),
+                resource->srv->srvconf.buffered_frames);
 
-    switch ( result ) {
-    case RESOURCE_OK:
-        break;
-    case RESOURCE_EOF:
-        if (!resource->eor) {
-            fnc_log(FNC_LOG_INFO,
-                    "r_read_cb: %s read_packet() end of file.",
+        switch(resource->demuxer->read_packet(resource)) {
+        case RESOURCE_OK:
+            break;
+        case RESOURCE_EOF:
+            if (!resource->eor) {
+                fnc_log(FNC_LOG_INFO,
+                        "r_read_cb: %s read_packet() end of file.",
+                        resource->info->mrl);
+                resource->eor = true;
+            }
+            break;
+        default:
+            fnc_log(FNC_LOG_FATAL,
+                    "r_fill_cb: %s read_packet() error.",
                     resource->info->mrl);
-            resource->eor = true;
+            goto stop;
         }
-        break;
-    default:
-        fnc_log(FNC_LOG_FATAL,
-                "r_read_cb: %s read_packet() error.",
-                resource->info->mrl);
+
+        fprintf(stderr, "(%u/%d)\n",
+                bq_consumer_unseen(consumer),
+                resource->srv->srvconf.buffered_frames);
     }
 
+ stop:
     g_mutex_unlock(resource->lock);
 }
 
@@ -179,8 +187,8 @@ Resource *r_open(struct feng *srv, const char *inner_path)
     r->lock = g_mutex_new();
 
     /* Create the new resource pool for the read requests */
-    r->read_pool = g_thread_pool_new(r_read_cb, r,
-                                     -1, false, NULL);
+    r->fill_pool = g_thread_pool_new(r_fill_cb, r,
+                                     1, false, NULL);
 
 #ifdef HAVE_METADATA
     cpd_find_request(srv, r, filename);
@@ -190,23 +198,24 @@ Resource *r_open(struct feng *srv, const char *inner_path)
 }
 
 /**
- * @brief Request reading data from a resource
+ * @brief Request filling the resource with data
  *
  * @param resource The resource to read from
- * @param count The number of iteration to read
+ * @param consumer The consumer to fill with data
  *
- * This function only pushes a read request on the threadpool of the
- * resource; the actual read will happen asynchronously.
+ * This function pushes read requests on the threadpool, they will
+ * take care of filling the track with data, asynchronously.
  *
- * @see r_read_cb
+ * @see r_fill_cb
  */
-void r_read(Resource *resource, gint count)
+void r_fill(Resource *resource, BufferQueue_Consumer *consumer)
 {
     g_mutex_lock(resource->lock);
 
-    g_thread_pool_push(resource->read_pool,
-                       GINT_TO_POINTER(count),
-                       NULL);
+    if ( !resource->eor )
+        g_thread_pool_push(resource->fill_pool,
+                           consumer,
+                           NULL);
 
     g_mutex_unlock(resource->lock);
 }
