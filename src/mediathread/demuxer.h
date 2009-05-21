5042a37f425df08e3112a6b287e2b0534b1f89bd
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2009-05-21 16:11:38 +0200

    Replace the r_read function with a r_fill instead.
    The r_fill function takes the consumer to fill up as an argument, and it
    repeats the read until the consumer is filled up with 16 unseen
    frames.
    
    This removes the assumption 1 packet -> 1 frame, and make sures that if
    both the audio and video consumers have to be filled up, it doesn't request
    reading of too many packages at once.

--- src/mediathread/demuxer.h
+++ src/mediathread/demuxer.h
@@ -80,7 +80,7 @@ ResourceInfo *resinfo_new();
 
 typedef struct Resource {
     GMutex *lock;
-    GThreadPool *read_pool;
+    GThreadPool *fill_pool;
     InputStream *i_stream;
     struct Demuxer *demuxer;
     ResourceInfo *info;
@@ -158,7 +158,7 @@ Demuxer *find_demuxer(InputStream *i_stream);
 
 Resource *r_open(struct feng *srv, const char *inner_path);
 
-void r_read(Resource *resource, gint count);
+void r_fill(Resource *resource, BufferQueue_Consumer *consumer);
 int r_seek(Resource *resource, double time);
 
 void r_close(Resource *resource);
