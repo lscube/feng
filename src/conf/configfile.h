f9180ac85c2633abf1704db4ded1520f86808ce3
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2010-06-04 19:56:04 +0200

    Remove commented-out and unused declarations.

--- src/conf/configfile.h
+++ src/conf/configfile.h
@@ -45,10 +45,5 @@ void configparserFree(void *p);
 void configparser(void *yyp, int yymajor, conf_buffer *yyminor, config_t *ctx);
 int config_parse_file(config_t *context, const char *fn);
 int config_read(const char *fn);
-//void config_cond_cache_reset(server *srv, connection *con);
-//void config_cond_cache_reset_item(server *srv, connection *con, comp_key_t item);
-
-#define config_cond_cache_reset_all_items(con) \
-    config_cond_cache_reset_item(con, COMP_LAST_ELEMENT);
 
 #endif
